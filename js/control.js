7c800336f8f46881c9e4c66782969e9918d43f68
Natalliax <Natalliax@NATALLIAX>
2023-03-21 22:10:31 +0300

    Добавьте файлы проекта.

---
+++ js/control.js
@@ -0,0 +1,10 @@
+import { startTimer } from "./timer.js";
+
+
+const btnStart = document.querySelector('.control__btn_start');
+
+export const initControl = () => {
+    btnStart.addEventListener('click', () => {
+        startTimer();
+    })
+}
